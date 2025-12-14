8b02594909c5b423e454b3d23ef09ad4dbd1b466
Jannatul <limajannat1675@gmail.com>
2025-12-14 23:45:20 +0600

    added MarketNews section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner";
 import CommunityReviews from "./components/CommunityReviews";
+import MarketNews from "./components/MarketNews";
 
 export default function Home() {
   return (
     <section>
       <div>
         <Banner/>
+        <MarketNews/>
         <CommunityReviews/>
       </div>
     </section>
