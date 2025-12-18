9bcfc296c969ff1d1c09078d2a7c643792b17ebc
Jannatul <limajannat1675@gmail.com>
2025-12-18 09:45:30 +0600

    added demo data

---
+++ src/data/products.ts
@@ -0,0 +1,35 @@
+export const products = [
+  {
+    id: "1",
+    name: "Potato",
+    category: "Vegetables",
+    market: "Karwan Bazar",
+    price: 45,
+    unit: "kg",
+    image: "/potato.jpg",
+    updatedAt: "10 min ago",
+    history: [40, 42, 43, 45]
+  },
+  {
+    id: "2",
+    name: "Onion",
+    category: "Vegetables",
+    market: "New Market",
+    price: 60,
+    unit: "kg",
+    image: "/onion.jpg",
+    updatedAt: "15 min ago",
+    history: [55, 58, 60]
+  },
+  {
+    id: "3",
+    name: "Hilsa Fish",
+    category: "Fish",
+    market: "Local Market",
+    price: 1200,
+    unit: "kg",
+    image: "/fish.jpg",
+    updatedAt: "30 min ago",
+    history: [1100, 1150, 1200]
+  },
+];
